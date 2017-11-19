b88cab542df31256880de3a73658256ed0219013
Mike DeLong <mikedelong@outlook.com>
2017-11-19 14:23:06 -0500

    cleanup: optimized imports.

--- basic.py
+++ basic.py
@@ -1,10 +1,10 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 import logging
 import time
+
+import matplotlib.pyplot as plt
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
 
 start_time = time.time()
 # set up logging
