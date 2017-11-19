bc5e517ec4572424decdfa408da4abb1f31c4c5c
Mike DeLong <mikedelong@outlook.com>
2017-11-18 19:59:24 -0500

    feature: initial checkin.

---
+++ basic.py
@@ -0,0 +1,29 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+import logging
+import time
+
+start_time = time.time()
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+data_path = './input/'
+data = pd.read_csv(data_path + 'pokemon.csv')
+
+logger.debug(data.info())
+
+
+logger.debug('done')
+elapsed_time = time.time() - start_time
+logger.debug('elapsed time %d seconds', elapsed_time)
