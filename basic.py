fca83a1087b877108ffb1233ca8367d1078d03de
Mike DeLong <mikedelong@outlook.com>
2017-11-20 20:26:47 -0500

    feature: a scatter plot, of attack vs. defense, with x min and y min = 0.

--- basic.py
+++ basic.py
@@ -24,20 +24,36 @@ logger.debug(data.info())
 
 figsize = (16, 9)
 figure, axes = plt.subplots(figsize=figsize)
-heatmap = sns.heatmap(data.corr(), annot=True, linewidths=.5, fmt= '.1f',ax=axes)
+heatmap = sns.heatmap(data.corr(), annot=True, linewidths=.5, fmt='.1f', ax=axes)
 plt.savefig('./pokemon_heatmap.png')
 del figure
+del axes
 
 # Line Plot
 # color = color, label = label, linewidth = width of line, alpha = opacity, grid = grid, linestyle = sytle of line
 figure, axes = plt.subplots(figsize=figsize)
-data.Speed.plot(kind = 'line', color = 'g',label = 'Attack',linewidth=1,alpha = 0.5,grid = True,linestyle = ':')
-data.Defense.plot(color = 'r',label = 'Defense',linewidth=1, alpha = 0.5,grid = True,linestyle = '-.')
-plt.legend(loc='upper right')     # legend = puts label into plot
-plt.xlabel('x axis')              # label = name of label
+data.Speed.plot(kind='line', color='g', label='Attack', linewidth=1, alpha=0.5, grid=True, linestyle=':')
+data.Defense.plot(color='r', label='Defense', linewidth=1, alpha=0.5, grid=True, linestyle='-.')
+plt.legend(loc='upper right')  # legend = puts label into plot
+plt.xlabel('x axis')  # label = name of label
 plt.ylabel('y axis')
-plt.title('Line Plot')            # title = title of plot
+plt.title('Line Plot')  # title = title of plot
 plt.savefig('./pokemon_speed_defense_plot.png')
+del figure
+del axes
+
+# Scatter Plot
+# x = attack, y = defense
+figure, axes = plt.subplots(figsize=figsize)
+data.plot(kind='scatter', x='Attack', y='Defense', alpha=0.5, color='red')
+plt.xlabel('Attack')  # label = name of label
+plt.ylabel('Defence')
+plt.xlim(xmin=0)
+plt.ylim(ymin=0)
+plt.title('Attack Defense Scatter Plot')  # title = title of plot
+plt.savefig('./pokemon_attack_defense_scatter_plot.png')
+del figure
+del axes
 
 logger.debug('done')
 elapsed_time = time.time() - start_time
