41b224502d3150dd37f7d4318bb5ed16dfc1f2c8
Mike DeLong <mikedelong@outlook.com>
2017-11-20 20:10:59 -0500

    feature: added a plot of speed and defense.

--- basic.py
+++ basic.py
@@ -26,7 +26,18 @@ figsize = (16, 9)
 figure, axes = plt.subplots(figsize=figsize)
 heatmap = sns.heatmap(data.corr(), annot=True, linewidths=.5, fmt= '.1f',ax=axes)
 plt.savefig('./pokemon_heatmap.png')
+del figure
 
+# Line Plot
+# color = color, label = label, linewidth = width of line, alpha = opacity, grid = grid, linestyle = sytle of line
+figure, axes = plt.subplots(figsize=figsize)
+data.Speed.plot(kind = 'line', color = 'g',label = 'Attack',linewidth=1,alpha = 0.5,grid = True,linestyle = ':')
+data.Defense.plot(color = 'r',label = 'Defense',linewidth=1, alpha = 0.5,grid = True,linestyle = '-.')
+plt.legend(loc='upper right')     # legend = puts label into plot
+plt.xlabel('x axis')              # label = name of label
+plt.ylabel('y axis')
+plt.title('Line Plot')            # title = title of plot
+plt.savefig('./pokemon_speed_defense_plot.png')
 
 logger.debug('done')
 elapsed_time = time.time() - start_time
