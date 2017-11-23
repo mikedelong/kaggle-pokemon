4ce6ab5afad2dc56630b1c40ad1e7f498fa1abf2
Mike DeLong <mikedelong@outlook.com>
2017-11-22 19:22:56 -0500

    cleanup: reformatted

--- basic.py
+++ basic.py
@@ -72,21 +72,21 @@ plt.savefig('./pokemon_attack_boxplot.png')
 del figure
 del axes
 
-
 figure, axes = plt.subplots(figsize=figsize)
-data.loc[:,["Attack","Defense","Speed"]].plot(subplots=True)
+data.loc[:, ["Attack", "Defense", "Speed"]].plot(subplots=True)
 plt.savefig('./pokemon_subplots.png')
 del figure
 del axes
 
 # histogram subplot with non cumulative and cumulative
-figure, axes = plt.subplots(nrows=2,ncols=1)
-data.loc[:,["Attack","Defense","Speed"]].plot(kind = "hist",y = "Defense",bins = 50,range= (0,250),normed = True,ax = axes[0])
-data.loc[:,["Attack","Defense","Speed"]].plot(kind = "hist",y = "Defense",bins = 50,range= (0,250),normed = True,ax = axes[1],cumulative = True)
+figure, axes = plt.subplots(nrows=2, ncols=1)
+data.loc[:, ["Attack", "Defense", "Speed"]].plot(kind="hist", y="Defense", bins=50, range=(0, 250), normed=True,
+                                                 ax=axes[0])
+data.loc[:, ["Attack", "Defense", "Speed"]].plot(kind="hist", y="Defense", bins=50, range=(0, 250), normed=True,
+                                                 ax=axes[1], cumulative=True)
 plt.savefig('./pokemon_histogram_subplots.png')
 del figure
 del axes
-
 
 elapsed_time = time.time() - start_time
 logger.debug('elapsed time %d seconds', elapsed_time)
