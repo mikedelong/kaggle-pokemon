2f8c0c3061971e0bd8c7ed8b73ea1dc51aa38742
Mike DeLong <mikedelong@outlook.com>
2017-11-22 19:22:06 -0500

    feature: added histogram subplots.

--- basic.py
+++ basic.py
@@ -79,5 +79,14 @@ plt.savefig('./pokemon_subplots.png')
 del figure
 del axes
 
+# histogram subplot with non cumulative and cumulative
+figure, axes = plt.subplots(nrows=2,ncols=1)
+data.loc[:,["Attack","Defense","Speed"]].plot(kind = "hist",y = "Defense",bins = 50,range= (0,250),normed = True,ax = axes[0])
+data.loc[:,["Attack","Defense","Speed"]].plot(kind = "hist",y = "Defense",bins = 50,range= (0,250),normed = True,ax = axes[1],cumulative = True)
+plt.savefig('./pokemon_histogram_subplots.png')
+del figure
+del axes
+
+
 elapsed_time = time.time() - start_time
 logger.debug('elapsed time %d seconds', elapsed_time)
